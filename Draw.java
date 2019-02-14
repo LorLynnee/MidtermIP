1eb82e8a72cdccdb4b0ceb699ab3ce2773dc0410
LorLynnee <germino1820308@mkt.ceu.edu.ph>
2019-02-14 14:11:14 +0800

    separate player

--- Draw.java
+++ Draw.java
@@ -24,6 +24,7 @@ public class Draw extends JComponent{
 	Monster monster2;
 	Monster2 monster3;
 	Monster3 monster4;
+	Player player;
 
 	public Draw(){
 
@@ -31,6 +32,7 @@ public class Draw extends JComponent{
 		monster2 = new Monster(300, 200);
 		monster3 = new Monster2(440, 150);
 		monster4 = new Monster3(500, 230);
+		player = new Player(180, 40);
 
 		try{
 			image = ImageIO.read(resource);
@@ -42,94 +44,6 @@ public class Draw extends JComponent{
     }
 
 
-	public void reloadImage(){
-		state++;
-
-		if(state == 0){
-			resource = getClass().getResource("run0.png");
-		}
-		else if(state == 1){
-			resource = getClass().getResource("run1.png");
-		}
-		else if(state == 2){
-			resource = getClass().getResource("run2.png");
-		}
-		else if(state == 3){
-			resource = getClass().getResource("run3.png");
-		}
-		else if(state == 4){
-			resource = getClass().getResource("run4.png");
-		}
-		else if(state == 5){
-			resource = getClass().getResource("run5.png");
-			state = 0;
-		}
-
-		try{
-			image = ImageIO.read(resource);
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-
-	public void attackAnimation(){
-		Thread thread1 = new Thread(new Runnable(){
-			public void run(){
-				for(int ctr = 0; ctr < 5; ctr++){
-					try {
-						if(ctr==4){
-							resource = getClass().getResource("attack0.png");
-						}
-						else{
-							resource = getClass().getResource("attack"+ctr+".png");
-						}
-						
-						try{
-							image = ImageIO.read(resource);
-						}
-						catch(IOException e){
-							e.printStackTrace();
-						}
-				        repaint();
-				        Thread.sleep(100);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
-		thread1.start();
-	}
-
-	public void attack(){
-		attackAnimation();
-	}
-
-	public void moveUp(){
-		y = y - 5;
-		reloadImage();
-		repaint();
-	}
-
-	public void moveDown(){
-		y = y + 5;
-		reloadImage();
-		repaint();
-	}
-
-	public void moveLeft(){
-		x = x - 5;
-		reloadImage();
-		repaint();
-	}
-
-	public void moveRight(){
-		x = x + 5;
-		reloadImage();
-		repaint();
-	}
-	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.YELLOW);
