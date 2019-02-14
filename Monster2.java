86f321ce23646e6ec04cbaf6de2f7e19982c9358
LorLynnee <germino1820308@mkt.ceu.edu.ph>
2019-02-14 13:34:32 +0800

    initialized midterms exam

---
+++ Monster2.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+public class Monster2{
+	
+	public int xPos = 150;
+	public int yPos = 300;
+
+	public BufferedImage image;
+	public URL resource = getClass().getResource("ghost1.png");
+
+	public Monster2(){
+		try{
+			image = ImageIO.read(resource);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public Monster2(int xPass, int yPass){
+		xPos = xPass;
+		yPos = yPass;
+
+		try{
+			image = ImageIO.read(resource);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
